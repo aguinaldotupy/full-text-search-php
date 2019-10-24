7384b1dcd7641f6b8cc04b8f928733198c7b82cb
Aguinaldo Tupy <a.tupy@webteam.pt>
2019-10-25 00:28:18 +0100

    First commit

---
+++ src/FullTextSearchServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tupy\FullTextSearch;
+
+use Illuminate\Support\ServiceProvider;
+
+class FullTextSearchServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
